dde27890b6eb643638ecde1d3487491c71095e2a
Santiago Rua <santiruab1@gmail.com>
2025-03-05 22:09:33 -0500

    Desarrollo finalizado actividad 22 feb backend 1

--- src/main/java/com/example/Jugador.java
+++ src/main/java/com/example/Jugador.java
@@ -1,8 +1,7 @@
 package com.example;
 
 // ¿Qué representa esta clase? ¿Qué información contiene?
-
-// Esta clase representa el objeto jugardor y contiene la información básica de este
+// Esta clase representa los futuros objetos jugardores y contiene la información básica de este.
 
 public class Jugador {
     private String nombre;
@@ -14,15 +13,21 @@ public class Jugador {
     // ¿Por qué es importante inicializar los atributos al crear un objeto? ¿Qué
     // pasaría si no usáramos un constructor?
     // Porque este metodo permite el acceso a la información desde la clase Main
-    // dado que los atributos de la clase Jugador son privadas
+    // dado que los atributos de la clase Jugador son privados
 
-    Jugador(String nombre, int edad, String posicion, int numeroCamiseta, String equipo) {
+    public Jugador(String nombre, int edad, String posicion, int numeroCamiseta, String equipo) {
         this.nombre = nombre;
         this.edad = edad;
         this.posicion = posicion;
         this.numeroCamiseta = numeroCamiseta;
         this.equipo = equipo;
     }
+
+    // ¿Por qué no accedemos directamente a los atributos desde otras clases? ¿Qué
+    // ventajas tiene usar getters y setters?
+    // Porque los atributos son privados y no se pueden acceder directamente desde
+    // otras clases. Los getters y setters permiten acceder a los atributos de la
+    // clase Jugador desde la clase Main.
 
     public String getNombre() {
         return nombre;
@@ -64,12 +69,24 @@ public class Jugador {
         this.equipo = equipo;
     }
 
+    // ¿Qué otros métodos podrían ser útiles para la clase Jugador? ¿Cómo podríamos mejorar este método?
+    // Otros métodos útiles para la clase Jugador podrían ser verificar si el jugador es mayor de edad, para saber si es apto para debutar en las ligas mayores, por ejemplo.
+    // Podríamos mejorar este método agregando un salto de linea para diferenciar las diferentes informaciones de los jugadores al momento de la impresion. Además, se puede mejorar agregando más información sobre el jugador, como su salario, su nacionalidad, etc. Igualmente esto implicaria crear nuevos atributos y métodos para la clase Jugador.
+
+
+    // Método para mostrar la información de un jugador
     public void mostrarInformacion() {
+        System.out.println("\n" + "**********");
         System.out.println("Nombre: " + nombre);
         System.out.println("Edad: " + edad);
         System.out.println("Posición: " + posicion);
         System.out.println("Número de Camiseta: " + numeroCamiseta);
         System.out.println("Equipo: " + equipo);
+    }
+
+    // Método para verificar si el jugador es mayor de edad
+    public boolean esMayorDeEdad() {
+        return this.edad >= 18;
     }
 
 }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,27 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        Jugador j1 = new Jugador(null, 0, null, 0, null);
+
+        // ¿Qué aprendiste al crear y utilizar objetos? ¿Cómo cambia el estado de un objeto cuando usamos métodos set? ¿Qué ventajas tiene inicializar los atributos de un objeto al crearlo?
+        // Aprendí que los objetos son instancias de una clase y que cada objeto puede tener propiedades diferentes. Al usar los métodos set, podemos actualizar el estado de un objeto. Las ventajas de inicializar los atributos de un objeto al crearlo son que podemos asignar valores iniciales a los atributos y asegurarnos de que el objeto tenga un estado válido desde el principio.
+        Jugador jugador1 = new Jugador ("Juan", 25, "Delantero", 9, "Real Madrid");
+        Jugador jugador2 = new Jugador ("Pedro", 22, "Defensa", 5, "Barcelona");
+        Jugador jugador3 = new Jugador ("Luis", 30, "Medio", 10, "Atletico de Madrid");
+        Jugador jugador4 = new Jugador ("Carlos", 27, "Portero", 1, "Sevilla");
+
+        jugador1.mostrarInformacion();
+        jugador2.mostrarInformacion();
+        jugador3.mostrarInformacion();
+        jugador4.mostrarInformacion();
+
+        jugador1.setEquipo("Atletico de Madrid");
+        jugador2.setEdad(23);
+        jugador3.setPosicion("Defensa");
+        jugador4.setNumeroCamiseta(13);
+
+        jugador1.mostrarInformacion();
+        jugador2.mostrarInformacion();
+        jugador3.mostrarInformacion();  
+        jugador4.mostrarInformacion();
     }
 }
